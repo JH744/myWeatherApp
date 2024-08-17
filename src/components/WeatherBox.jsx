a094238fa832055837d91be8c007e76c76ada010
JiHun Kim <dydekffrl@naver.com>
2024-08-18 04:26:29 +0900

    dev 브랜치생성

--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -5,7 +5,7 @@ const WeatherBox = ({ weather }) => {
   return (
     <div className="box">
       <div>{weather?.name}</div>
-      <h2>{weather?.main.temp}도 / 230화씨</h2>
+      <h2>{weather?.main.temp}도/230화씨</h2>
       <h3>{weather?.weather[0].main}</h3>
     </div>
   );
